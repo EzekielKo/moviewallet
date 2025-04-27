f6168b2a8181132da310781c737fda6763e22ec1
EzekielKo <118594978+EzekielKo@users.noreply.github.com>
2025-04-27 12:42:29 +1200

    homescreenui

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,8 +3,10 @@ import { icons } from "@/constants/icons";
 import {images} from "@/constants/images";
 import { Link } from "expo-router";
 import { Text, View, Image, ScrollView } from "react-native";
+import { useRouter } from 'expo-router'
 
 export default function Index() {
+  const router = useRouter();
   return (
     <View className="flex-1 bg-primary">
       <Image source={images.bg} className="absolute w-full z-0"/>
@@ -15,7 +17,10 @@ export default function Index() {
       >
         <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto"/>
         <View className="flex-1 px-5">
-          <SearchBar/>
+          <SearchBar
+            onPress={() => router.push("/search")}
+            placeholder="Search for a movie"
+          />
         </View>
       </ScrollView>
     </View>
