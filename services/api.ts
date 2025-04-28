efa94547329a9103486d4eab725f7f3a91f87360
EzekielKo <118594978+EzekielKo@users.noreply.github.com>
2025-04-28 15:47:52 +1200

    created api service

---
+++ services/api.ts
@@ -0,0 +1,13 @@
+// const url = 'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc';
+// const options = {
+//   method: 'GET',
+//   headers: {
+//     accept: 'application/json',
+//     Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiOTFiZDlhYmQzNzJjMmRlNWRmNzVlNTVhZmE5ZDBlNiIsIm5iZiI6MTc0NTgxMTM5MC42NTQsInN1YiI6IjY4MGVmN2JlNWE5Yjc4ZDI3MjgxM2ZlOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.uyi-8gyzH_Ocx-7VsiOVJy36_RaYR0QuxZAQuTi8W40'
+//   }
+// };
+
+// fetch(url, options)
+//   .then(res => res.json())
+//   .then(json => console.log(json))
+//   .catch(err => console.error(err));
