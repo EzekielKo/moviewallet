924eb71d7201671dbb29bc51aa381ab4e9748238
EzekielKo <118594978+EzekielKo@users.noreply.github.com>
2025-05-01 09:43:01 +1200

    edit index.tsx

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,11 +2,22 @@ import SearchBar from "@/components/SearchBar";
 import { icons } from "@/constants/icons";
 import {images} from "@/constants/images";
 import { Link } from "expo-router";
-import { Text, View, Image, ScrollView } from "react-native";
+import { Text, View, Image, ScrollView, ActivityIndicator } from "react-native";
 import { useRouter } from 'expo-router'
+import useFetch from "@/services/useFetch";
+import { fetchMovies } from "@/services/api";
 
 export default function Index() {
   const router = useRouter();
+
+  const {
+    data: movies, 
+    loading: moviesLoading, 
+    error: moviesError
+  } = useFetch(() => fetchMovies({
+    query: ''
+  }))
+
   return (
     <View className="flex-1 bg-primary">
       <Image source={images.bg} className="absolute w-full z-0"/>
@@ -16,12 +27,27 @@ export default function Index() {
         showsVerticalScrollIndicator={false} contentContainerStyle={{minHeight: "100%", paddingBottom: 10}}
       >
         <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto"/>
-        <View className="flex-1 px-5">
-          <SearchBar
-            onPress={() => router.push("/search")}
-            placeholder="Search for a movie"
+
+        {moviesLoading ? (
+          <ActivityIndicator
+            size="large"
+            color="#0000ff"
+            className="mt-10 self-center"
           />
-        </View>
+        ) : moviesError ? (
+          <Text>Error: {moviesError?.message}</Text>
+        ) : (
+          <View className="flex-1 px-5">
+            <SearchBar
+              onPress={() => router.push("/search")}
+              placeholder="Search for a movie"
+            />
+            <>
+              <Text className="text-lg text-white font-bold mt-5 mb-3">Latest Movies</Text>
+            </>
+          </View>
+        )}
+
       </ScrollView>
     </View>
   );
