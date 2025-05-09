17610876f65c885c57f2c12b3604b34d4e8282de
EzekielKo <118594978+EzekielKo@users.noreply.github.com>
2025-05-09 15:44:25 +1200

    remove router

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -3,18 +3,14 @@ import React from 'react'
 import { images } from '@/constants/images'
 import MovieCard from '@/components/MovieCard'
 import { fetchMovies } from '@/services/api'
-import { useRouter } from 'expo-router'
 import useFetch from '@/services/useFetch'
 import { icons } from '@/constants/icons'
 import SearchBar from '@/components/SearchBar'
-import { loadOptions } from '@babel/core'
 
 const Search = () => {
-  const router = useRouter();
+  
   const {
-    data: movies, 
-    loading, 
-    error
+    data: movies, loading, error
   } = useFetch(() => fetchMovies({
     query: ''
   }))
@@ -38,15 +34,19 @@ const Search = () => {
               <Image source={icons.logo} className="w-12 h-10"/>
             </View>
             <View className="my-5">
-              <SearchBar placeholder='Search movies...'/>
+              <SearchBar 
+                placeholder='Search movies...'
+              />
             </View>
 
             {loading && (
               <ActivityIndicator size="large" color="#0000ff"/>
             )}
+
             {error && (
               <Text className="text-red-500 px-5 my-3">Error: {error.message}</Text>
             )}
+
             {!loading && !error && 'SEARCH TERM'.trim() && movies?.length > 0 && (
               <Text className="text-xl text-white font-bold">
                 Search Results for{' '}
