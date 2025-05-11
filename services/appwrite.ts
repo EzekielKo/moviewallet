baed2462e4188053700046c0095b4674f2abf80b
EzekielKo <118594978+EzekielKo@users.noreply.github.com>
2025-05-11 17:39:03 +1200

    connect with apprtire

--- services/appwrite.ts
+++ services/appwrite.ts
@@ -1,4 +1,5 @@
-import {Client, Databases} from "react-native-appwrite"
+import { Client, Databases, ID, Query } from "appwrite";
+
 const DATABSE_ID = process.env.EXPO_PUBLIC_APPWRITE_DATABASE_ID!;
 const COLLECTION_ID = process.env.EXPO_PUBLIC_APPWRITE_COLLECTION_ID!;
 
@@ -9,5 +10,34 @@ const client = new Client()
 const database = new Databases(client);
 
 export const updateSearchCount = async (query: string, movie: Movie) => {
-    
+    try {
+        const result = await database.listDocuments(DATABSE_ID, COLLECTION_ID, [
+            Query.equal('searchTerm', query)
+        ])
+        console.log(result)
+
+        if(result.documents.length > 0) {
+            const existingMovie = result.documents[0];
+
+            await database.updateDocument(
+                DATABSE_ID, 
+                COLLECTION_ID, 
+                existingMovie.$id,
+                {
+                    count: existingMovie.count + 1
+                }
+            )
+        } else {
+            await database.createDocument(DATABSE_ID, COLLECTION_ID, ID.unique(), {
+                title: movie.title,
+                searchTerm: query,
+                movie_id: movie.id,
+                count: 1,
+                poster_url: `https://image.tmdb.org/t/p/w500${movie.poster_path}`
+            })
+        }
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
 }
