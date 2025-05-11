345f5f5f547d3361a7681911383e033bb0904a0d
EzekielKo <118594978+EzekielKo@users.noreply.github.com>
2025-05-11 16:53:35 +1200

    create client

--- services/appwrite.ts
+++ services/appwrite.ts
@@ -1,5 +1,10 @@
+import {Client} from "react-native-appwrite"
 const DATABSE_ID = process.env.EXPO_PUBLIC_APPWRITE_DATABASE_ID!;
 const COLLECTION_ID = process.env.EXPO_PUBLIC_APPWRITE_COLLECTION_ID!;
+
+const client = new Client()
+    .setEndpoint("https://cloud.appwrite.io/v1")
+    .setProject(process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID!)
 
 export const updateSearchCount = async (query: string, movie: Movie) => {
     
