6cda7476a366d1f1f303383e4667fd1eb6a736d9
EzekielKo <118594978+EzekielKo@users.noreply.github.com>
2025-05-11 16:50:56 +1200

    import database and colleciton ids

---
+++ services/appwrite.ts
@@ -0,0 +1,6 @@
+const DATABSE_ID = process.env.EXPO_PUBLIC_APPWRITE_DATABASE_ID!;
+const COLLECTION_ID = process.env.EXPO_PUBLIC_APPWRITE_COLLECTION_ID!;
+
+export const updateSearchCount = async (query: string, movie: Movie) => {
+    
+}
