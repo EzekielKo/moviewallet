f1c0b85e58e0a3bdcc6bdc97495bdf2703448b0d
EzekielKo <118594978+EzekielKo@users.noreply.github.com>
2025-05-13 21:28:31 +1200

    update search useEffect

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -17,21 +17,22 @@ const Search = () => {
   }),false )
 
   useEffect(() => {
-    
     const timeoutID = setTimeout(async () => {
       if(searchQuery.trim()){
         await loadMovies();
-        if(movies?.length > 0 && movies?.[0]) {
-          await updateSearchCount(searchQuery, movies[0]);
-        }
 
       } else {
         reset()
       }
     }, 1000);
-
     return () => clearTimeout(timeoutID);
   }, [searchQuery])
+
+  useEffect(() => {
+    if(movies?.length > 0 && movies?.[0]) {
+      updateSearchCount(searchQuery, movies[0]);
+    }
+  }, [movies])
 
   return (
     <View className="flex-1 bg-primary">
