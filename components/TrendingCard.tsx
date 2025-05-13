6f7496b65a8c54d5d084de048a79947a7028bf68
EzekielKo <118594978+EzekielKo@users.noreply.github.com>
2025-05-13 21:43:26 +1200

    create TrendingCard

---
+++ components/TrendingCard.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { Link } from 'expo-router'
+
+const TrendingCard = ({movie: {movie_id, title, poster_url}, index}: TrendingCardProps) => {
+  return (
+    <Link href={`/movies/${movie_id}`} asChild>
+        
+    </Link>
+  )
+}
+
+export default TrendingCard
